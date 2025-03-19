47ddbb6409664ce717b2482fe2cfd5c685820060
Matias Alessandrello <matialess@gmail.com>
2025-03-19 14:59:24 -0300

    ejercicio 6

---
+++ ejercicio_6.py
@@ -0,0 +1,15 @@
+def tabla_multiplicacion(numero):
+    print(numero*1)
+    print(numero*2)
+    print(numero*3)
+    print(numero*4)
+    print(numero*5)
+    print(numero*6)
+    print(numero*7)
+    print(numero*8)
+    print(numero*9)
+    print(numero*10)
+    
+numero_multiplicar = int(input("ingrese un numero para multiplicar "))
+
+tabla_multiplicacion(numero_multiplicar)
